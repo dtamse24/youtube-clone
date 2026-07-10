ff6e21e431d549731911fd9bd9262bce1aad41b6
Darshan Tamse <darshan.tamse@gmail.com>
2026-07-10 14:50:28 +0530

    Initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Navbar.css'
+import menu_icon from '../../assets/menu.png'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search.png'
+import upload_icon from '../../assets/upload.png'
+import more_icon from '../../assets/more.png'
+import notification_icon from '../../assets/notification.png'
+import profile_icon from '../../assets/jack.png'
+import { Link } from 'react-router-dom'
+
+const Navbar = ({setSidebar}) => {
+  return (
+    <nav className='flex-div'>
+        <div className="nav-left flex-div">
+            {/* <img className='menu-icon' onClick={() => setSidebar(prev => !prev)} src={menu_icon} alt="Menu" /> */}
+            <img className='menu-icon' onClick={()=>setSidebar(prev=>prev===false?true:false)} src={menu_icon} alt="Menu" />
+            <Link to="/"><img className='logo' src={logo} alt="VidTube Logo" /></Link>
+        </div>
+
+        <div className="nav-middle flex-div">
+            <div className="search-box flex-div">
+              <input type="text" placeholder="Search" />
+              <img className='search-icon' src={search_icon} alt="Search" />      
+            </div>
+        </div>
+
+        <div className="nav-right flex-div">
+            <img src={upload_icon} alt="Upload" />
+            <img src={more_icon} alt="More" />
+            <img src={notification_icon} alt="Notification" />
+            <img src={profile_icon} alt="Profile" className='user-icon'/>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
+
+console.log(import.meta.env.VITE_YOUTUBE_API_KEY);
+
+export const value_converter = (value) => {
+    if(value>=1000000000){
+        return Math.floor(value/1000000000)+"B";
+    }
+    else if(value>=1000000){
+        return Math.floor(value/1000000)+"M";
+    }
+    else if(value>=1000){
+        return Math.floor(value/1000)+"K";
+    }
+    else{
+        return value;
+    }
+}    
